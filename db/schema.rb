4731409a740ba8a552b723edc37887ea864932d8
guillaume imary <guillaume.imary@gmail.com>
2025-08-10 17:09:27 +0200

    Update schema for sessions.price
    Align schema.rb with new migration adding price to sessions.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_03_124432) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_10_122158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_03_124432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "terrain"
+    t.decimal "price", precision: 8, scale: 2, default: "0.0", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
