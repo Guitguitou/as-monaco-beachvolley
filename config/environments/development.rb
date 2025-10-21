69b0dd25ece0f75ba59d7f40ba23ec44c437f426
guillaume imary <guillaume.imary@gmail.com>
2025-10-21 18:08:26 +0200

    feat: migration de Solid Queue vers Sidekiq dans les environnements
    - Production: utilise Sidekiq comme queue adapter
    - Development: utilise Sidekiq comme queue adapter
    - Test: utilise l'adapter :test pour exécution inline
    - Suppression des configurations Solid Queue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
 
+  # Use Sidekiq for Active Job in development
+  config.active_job.queue_adapter = :sidekiq
+
   # Store uploaded files using Cloudinary in development as well (override via ENV if needed).
   config.active_storage.service = (ENV["ACTIVE_STORAGE_SERVICE"].presence || :cloudinary).to_sym
 
