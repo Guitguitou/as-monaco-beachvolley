a4abf8a1b2a97871b9b9dd07464095f06c2c2019
guillaume imary <guillaume.imary@gmail.com>
2025-10-22 09:32:13 +0200

    feat(webhook): add webhook endpoint + callback job/service + post-payment job (Sidekiq)
    - Webhook Sherlock avec vérification signature HMAC
    - SherlockCallbackJob pour traitement asynchrone
    - Service HandleCallback pour normaliser et traiter les statuts
    - PostPaymentFulfillmentJob pour actions post-paiement
    - Gestion des callbacks success/failed/cancelled

---
+++ app/jobs/post_payment_fulfillment_job.rb
@@ -0,0 +1,14 @@
+class PostPaymentFulfillmentJob < ApplicationJob
+  queue_as :default
+
+  def perform(credit_purchase_id)
+    credit_purchase = CreditPurchase.find(credit_purchase_id)
+    
+    # TODO: Envoyer email de confirmation
+    # UserMailer.payment_confirmation(credit_purchase).deliver_later
+    
+    # TODO: Notifier Sentry/Analytics
+    Rails.logger.info("Payment fulfilled for CreditPurchase ##{credit_purchase.id} - User ##{credit_purchase.user_id} received #{credit_purchase.credits} credits")
+  end
+end
+
---
+++ app/jobs/sherlock_callback_job.rb
@@ -0,0 +1,8 @@
+class SherlockCallbackJob < ApplicationJob
+  queue_as :default
+
+  def perform(callback_params)
+    Sherlock::HandleCallback.new(callback_params).call
+  end
+end
+
