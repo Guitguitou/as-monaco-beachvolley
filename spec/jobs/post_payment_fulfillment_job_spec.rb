948ba00a578046b22a4e1c8d174d0e8ae8689393
guillaume imary <guillaume.imary@gmail.com>
2025-12-02 08:47:29 +0100

    test: Add tests for helpers and jobs
    - Add tests for ApplicationHelper (level badges, session types, transaction types, external URLs)
    - Add tests for PostPaymentFulfillmentJob (payment fulfillment logging)
    - Add tests for SherlockCallbackJob (webhook job processing)

---
+++ spec/jobs/post_payment_fulfillment_job_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PostPaymentFulfillmentJob, type: :job do
+  let(:user) { create(:user) }
+  let(:credit_purchase) { create(:credit_purchase, :paid, user: user, credits: 1000) }
+
+  before do
+    allow(Rails.logger).to receive(:info)
+  end
+
+  describe '#perform' do
+    it 'finds the credit purchase' do
+      expect(CreditPurchase).to receive(:find).with(credit_purchase.id).and_return(credit_purchase)
+      
+      described_class.perform_now(credit_purchase.id)
+    end
+
+    it 'logs payment fulfillment information' do
+      expect(Rails.logger).to receive(:info).with(
+        "Payment fulfilled for CreditPurchase ##{credit_purchase.id} - User ##{user.id} received #{credit_purchase.credits} credits"
+      )
+      
+      described_class.perform_now(credit_purchase.id)
+    end
+
+    it 'handles missing credit purchase gracefully' do
+      expect {
+        described_class.perform_now(999999)
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
+
