4691602c0a4ef36c80fb527416910a8e14287f3e
guillaume imary <guillaume.imary@gmail.com>
2025-12-05 15:26:01 +0100

    refactor: extract coach data logic into CoachDataService
    - Create CoachDataService to centralize coach data loading
    - Remove duplicated coach data methods from Admin::UsersController and UsersController
    - Simplify controllers by delegating to service
    - Follow Captive standards: DRY, single responsibility, testable code

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -151,59 +151,23 @@ module Admin
     end
 
     def load_coach_data_for_admin
-      week_range  = Time.zone.today.beginning_of_week..(Time.zone.today.beginning_of_week + 7.days)
-      month_range = Time.zone.now.beginning_of_month..Time.zone.now.end_of_month
-      year_range  = Time.zone.now.beginning_of_year..Time.zone.now.end_of_year
+      coach_data = CoachDataService.new(@user)
+      counts = coach_data.training_counts
+      salaries = coach_data.salaries
 
-      @my_trainings_week_count  = Session.where(user_id: @user.id, session_type: 'entrainement',
-                                                start_at: week_range).count
-      @my_trainings_month_count = Session.where(user_id: @user.id, session_type: 'entrainement',
-                                                start_at: month_range).count
-      @my_trainings_year_count  = Session.where(user_id: @user.id, session_type: 'entrainement',
-                                                start_at: year_range).count
-
-      spt = @user.salary_per_training
-      @my_salary_week  = (@my_trainings_week_count  * spt).to_f
-      @my_salary_month = (@my_trainings_month_count * spt).to_f
-      @my_salary_year  = (@my_trainings_year_count  * spt).to_f
+      @my_trainings_week_count = counts[:week]
+      @my_trainings_month_count = counts[:month]
+      @my_trainings_year_count = counts[:year]
+      @my_salary_week = salaries[:week]
+      @my_salary_month = salaries[:month]
+      @my_salary_year = salaries[:year]
     end
 
     def load_coach_trainings_data_for_admin
-      # Past trainings with details
-      @past_trainings = Session.includes(:levels, :registrations)
-                               .where(user_id: @user.id, session_type: 'entrainement')
-                               .where('start_at < ?', Time.current)
-                               .order(start_at: :desc)
-                               .limit(50)
-
-      # Upcoming trainings
-      @upcoming_trainings = Session.includes(:levels, :registrations)
-                                   .where(user_id: @user.id, session_type: 'entrainement')
-                                   .where('start_at >= ?', Time.current)
-                                   .order(start_at: :asc)
-                                   .limit(20)
-
-      # Monthly salary data for the last 12 months
-      @monthly_salary_data = []
-      12.times do |i|
-        month_start = (Time.current - i.months).beginning_of_month
-        month_end = month_start.end_of_month
-
-        training_count = Session.where(
-          user_id: @user.id,
-          session_type: 'entrainement',
-          start_at: month_start..month_end
-        ).count
-
-        total_salary = training_count * @user.salary_per_training
-
-        @monthly_salary_data << {
-          month_name: month_start.strftime('%B %Y'),
-          training_count:,
-          total_salary:
-        }
-      end
-      @monthly_salary_data.reverse! # Show oldest to newest
+      coach_data = CoachDataService.new(@user)
+      @past_trainings = coach_data.past_trainings
+      @upcoming_trainings = coach_data.upcoming_trainings
+      @monthly_salary_data = coach_data.monthly_salary_data
     end
 
     def user_params
