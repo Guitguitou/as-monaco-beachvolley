7ffcec860db210de02d6e006da356603fcb13ebd
guillaume imary <guillaume.imary@gmail.com>
2026-01-05 17:19:11 +0100

    🚫 Ajouter filtre 'Test' aux méthodes restantes
    - Exclure les utilisateurs 'Test' dans top_player_by_sessions
    - Exclure les utilisateurs 'Test' dans top_player_by_sessions_in_period

--- app/services/stats/performance_dashboard.rb
+++ app/services/stats/performance_dashboard.rb
@@ -120,6 +120,7 @@ module Stats
         .valid
         .joins(:user, :session)
         .where(users: { id: user_ids })
+        .where.not(users: { last_name: "Test" })
         .group("users.id", "users.first_name", "users.last_name")
         .order("COUNT(registrations.id) DESC, MIN(registrations.created_at) ASC")
         .limit(3)
@@ -155,6 +156,7 @@ module Stats
         .valid
         .joins(:user)
         .where(users: { id: user_ids })
+        .where.not(users: { last_name: "Test" })
         .where(session_id: session_ids)
         .group("users.id", "users.first_name", "users.last_name")
         .order("COUNT(registrations.id) DESC, MIN(registrations.created_at) ASC")
