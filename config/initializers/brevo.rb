593eba93923900b674858fe9c4665eea4dbdd1cf
guillaume imary <guillaume.imary@gmail.com>
2026-02-20 11:30:35 +0100

    WIP

--- config/initializers/brevo.rb
+++ config/initializers/brevo.rb
@@ -6,7 +6,6 @@ if ENV["BREVO_API_KEY"].present?
   Brevo.configure do |config|
     config.api_key["api-key"] = ENV["BREVO_API_KEY"]
     config.timeout = 10
-    config.user_agent = "as-monaco-beach-volley/transactional-email"
   end
 else
   Rails.logger&.warn("[Brevo] BREVO_API_KEY is not set; transactional emails are disabled")
